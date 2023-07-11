82fac97cac3bc70fc4ad8906376702ac8717fbfd
mhdianrush <mhdianrush@gmail.com>
2023-07-11 16:28:31 +0700

    Constructed for Index, Find, Create, Update, and Delete Function

---
+++ controllers/product.controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Find(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
