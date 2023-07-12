d593d5d1b2dde8922caf463d9a76faa6f1a06c56
mhdianrush <mhdianrush@gmail.com>
2023-07-12 07:12:08 +0700

    Developed for Index Funtion to Find All List of Data, and Find Function to Find Data by Id

--- controllers/product.controller.go
+++ controllers/product.controller.go
@@ -1,13 +1,47 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/mhdianrush/go-products-rest-api/config"
+	"github.com/mhdianrush/go-products-rest-api/entities"
+	"github.com/mhdianrush/go-products-rest-api/helper"
+	"gorm.io/gorm"
+)
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	var products []entities.Product
 
+	if err := config.DB.Find(&products).Error; err != nil {
+		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	helper.ResponseJSON(w, http.StatusOK, products)
 }
 
 func Find(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		helper.ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
 
+	var product entities.Product
+
+	if err := config.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			helper.ResponseError(w, http.StatusNotFound, "product not found")
+			return
+		default:
+			helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+	helper.ResponseJSON(w, http.StatusOK, product)
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
